ee7b860b1baa43e10226c28124a8e70370336814
Ericheel <283773833@qq.com>
2018-05-17 22:47:13 +0800

    增加判断是否是保护期的工具类方法

--- src/main/java/org/wuancake/utils/WeekNumUtils.java
+++ src/main/java/org/wuancake/utils/WeekNumUtils.java
@@ -1,7 +1,9 @@
 package org.wuancake.utils;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class WeekNumUtils {
@@ -33,13 +35,43 @@ public class WeekNumUtils {
 
     /**
      * 判断是否是保护期
+     * true 是保护期 false 不是保护期
      *
      * @return
      */
     public static Boolean isProtected(Date time2Wuanlife) {
-       
+        /**
+         * 判断思路，加入午安时间到那周末的天数和加入午安时间和现在天数作比较
+         */
+        //加入时间转化到Calendar
+        Calendar instance = Calendar.getInstance();
+        instance.setTime(time2Wuanlife);
 
-        return true;
+        //加入时间是那周的星期几
+        int days = instance.get(Calendar.DAY_OF_WEEK) - 1;
+        if (days == 0) {
+            days = 7;
+        }
+
+        //加入那周的下周一的日期
+        instance.add(Calendar.DAY_OF_WEEK, 8 - days);
+        Date date2NextWeek = instance.getTime();
+
+        //日期格式化类
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        //当前的标准时间，格式化，排除掉具体的时分秒
+        Date nowTime = new Date();
+
+        //现在的时间（需求的）
+        try {
+            nowTime = simpleDateFormat.parse(simpleDateFormat.format(nowTime));
+            date2NextWeek = simpleDateFormat.parse(simpleDateFormat.format(date2NextWeek));
+        } catch (ParseException e) {
+            //时间转化异常
+
+        }
+
+        return nowTime.compareTo(date2NextWeek) < 0;
     }
 
 }
---
+++ src/test/java/org/wuancake/admin_back/Test.java
@@ -0,0 +1,65 @@
+package org.wuancake.admin_back;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Test {
+
+    public void test1() {
+        //加入时间
+        Date enterTime = new Date("2018/5/1");
+
+        Calendar instance = Calendar.getInstance();
+        Date nowTime = instance.getTime();//当前时间
+
+        instance.setTime(enterTime);
+
+        int dayOfWeek = instance.get(Calendar.DAY_OF_WEEK) - 1;
+        if (dayOfWeek == 0) {
+            dayOfWeek = 7;
+        }
+        System.out.println("加入的时候是星期" + dayOfWeek);
+
+        Long days2NextWeek = (8 - dayOfWeek) * 1000L * 60 * 60 * 24;
+
+        Date nextWeekDate = new Date(enterTime.getTime() + days2NextWeek);
+
+        System.out.println(nextWeekDate.toLocaleString());
+    }
+
+    public void test2() {
+        Date enterTime = new Date();
+
+        Calendar instance = Calendar.getInstance();
+
+        instance.setTime(enterTime);
+
+        int days = instance.get(Calendar.DAY_OF_WEEK) - 1;
+        if (days == 0) {
+            days = 7;
+        }
+        instance.add(Calendar.DAY_OF_WEEK, 8 - days);
+
+        Date date2NextWeek = instance.getTime();
+
+        Date nowTime = new Date("2018/5/20");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String format = simpleDateFormat.format(nowTime);
+        Date parse = null;
+        try {
+            parse = simpleDateFormat.parse(format);
+            date2NextWeek = simpleDateFormat.parse(simpleDateFormat.format(date2NextWeek));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("date2NextWeek:" + date2NextWeek.toLocaleString());
+        System.out.println("nowTime:" + parse.toLocaleString());
+
+        System.out.println(nowTime.compareTo(date2NextWeek));
+
+    }
+}
