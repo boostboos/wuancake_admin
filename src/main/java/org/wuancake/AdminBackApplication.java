df0a1c47d49b929944e0b4f86ee0b44db4fd6c1d
Ericheel <283773833@qq.com>
2018-06-15 17:43:28 +0800

    配置方面修改

--- src/main/java/org/wuancake/AdminBackApplication.java
+++ src/main/java/org/wuancake/AdminBackApplication.java
@@ -2,7 +2,11 @@ package org.wuancake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,4 +19,5 @@ public class AdminBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminBackApplication.class, args);
     }
+
 }
