1d35161399e991aa7d35f5fe0a35d0a6a72f51b7
maggictang <894144874@qq.com>
2018-06-27 18:15:07 +0800

    修改了表格行列宽局，看起来更合理而不是所有列间距都一个大小

--- src/main/java/org/wuancake/AdminBackApplication.java
+++ src/main/java/org/wuancake/AdminBackApplication.java
@@ -2,11 +2,7 @@ package org.wuancake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
--- src/main/java/org/wuancake/controller/ReportController.java
+++ src/main/java/org/wuancake/controller/ReportController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.wuancake.entity.AdminBean;
 import org.wuancake.entity.ReportBean;
