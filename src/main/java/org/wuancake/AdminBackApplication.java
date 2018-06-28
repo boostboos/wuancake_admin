2c448bc0213eb639a84c2e3d352a3b7e2bb0507e
Ericheel <283773833@qq.com>
2018-06-28 19:25:38 +0800

    引用外部配置文件

--- src/main/java/org/wuancake/AdminBackApplication.java
+++ src/main/java/org/wuancake/AdminBackApplication.java
@@ -3,6 +3,7 @@ package org.wuancake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
