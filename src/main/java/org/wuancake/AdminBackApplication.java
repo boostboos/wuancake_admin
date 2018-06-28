cbad2b1afe55fc61c74fcb6e559c595a44cca38d
Ericheel <283773833@qq.com>
2018-06-28 19:25:08 +0800

    引用外部配置文件

--- src/main/java/org/wuancake/AdminBackApplication.java
+++ src/main/java/org/wuancake/AdminBackApplication.java
@@ -14,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableCaching
+@PropertySource("file:/etc/application.yml")
 public class AdminBackApplication {
 
     public static void main(String[] args) {
